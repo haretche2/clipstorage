16b6a4c37a2ba83d2303a0129476acf019445b3c
Santiago Haretche <tatoharetche@hotmail.com>
2013-06-04 20:19:40 -0300

    Intento 1 recuperar fallo git

--- src/main/java/edu/umflix/clipstorage/tools/StorageServerTools.java
+++ src/main/java/edu/umflix/clipstorage/tools/StorageServerTools.java
@@ -2,12 +2,28 @@ package edu.umflix.clipstorage.tools;
 
 import edu.umflix.clipstorage.Exceptions.ClipStorageConfiguracionIncompletaException;
 import edu.umflix.clipstorage.config.Configuration;
+import edu.umflix.clipstorage.model.ClipDataLocation;
 import edu.umflix.clipstorage.model.StorageServer;
+import edu.umflix.model.ClipData;
 import javassist.bytecode.ExceptionsAttribute;
+import org.apache.commons.net.ftp.FTPClient;
 import org.apache.log4j.Logger;
+import sun.misc.IOUtils;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+
 
 /**
  * Created with IntelliJ IDEA.
@@ -17,22 +33,15 @@ import java.util.List;
  * To change this template use File | Settings | File Templates.
  */
 public class StorageServerTools {
-
     private static Logger log= Logger.getLogger(StorageServerTools.class);
+    private static Random randomGenerator=new Random();
 
-    public static List<StorageServer> getServidoresParaAlmacenarClip(List<StorageServer> disponibles){
-        List<StorageServer> result= new ArrayList<StorageServer>();
-        for(int i=0;i< Configuration.getIntConfiguration("Replicas");i++){
-            if(disponibles.size()>0){
-                StorageServer siguiente=getSiguienteServidoresParaAlmacenarClip(disponibles);
-                result.add(siguiente);
-                disponibles.remove(siguiente);
-            }else {
-                log.warn("Insuficientes servidores de almacenamiento");
-            }
-        }
-        return result;
+    public static void servidorCaido(StorageServer servidorCaido){
+        servidorCaido.setOnline(false);
+
+
     }
+
     public static StorageServer getSiguienteServidoresParaAlmacenarClip(List<StorageServer> disponibles){
         StorageServer elDeMenorEspacioOcupado=null;
         for(StorageServer actual:disponibles){
@@ -46,5 +55,36 @@ public class StorageServerTools {
             throw new ClipStorageConfiguracionIncompletaException("No hay mas servidores online disponibles");
         }
 
+    }
+
+    public static void guardar(StorageServer servidorEnQueAlmacenar, ClipData datos) throws IOException {
+        FTPClient clienteFtp = FtpTools.getClient(servidorEnQueAlmacenar);
+        byte[] bytes= new byte[datos.getBytes().length];
+        for(int i=0;i<bytes.length;i++){
+            bytes[i]=datos.getBytes()[i];
+        }
+        clienteFtp.storeFile((datos.getClip().getId()).toString(), new ByteArrayInputStream(bytes));
+        ClipDataLocation clipDataLocation=new ClipDataLocation();
+        clipDataLocation.setClipId(datos.getClip().getId());
+        clipDataLocation.setServidor(servidorEnQueAlmacenar);
+        Memory.addClipDataLocation(clipDataLocation);
+    }
+
+    public static Byte[] leer(StorageServer servidorDelQueLeer, long fileName) throws IOException {
+        FTPClient clienteFtp = FtpTools.getClient(servidorDelQueLeer);
+        ByteArrayOutputStream outputStream = null;
+        clienteFtp.retrieveFile(Long.toString(fileName), outputStream);
+        byte[] bytes=outputStream.toByteArray();
+        Byte[] convertedBytes= new Byte[bytes.length];
+        for(int i=0;i<bytes.length;i++){
+            convertedBytes[i]=bytes[i];
+        }
+        return convertedBytes;
+    }
+
+    public static StorageServer getRandomFromList(List<StorageServer> list)    {
+        int index = randomGenerator.nextInt(list.size());
+        StorageServer item = list.get(index);
+        return item;
     }
 }
